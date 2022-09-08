2d3669df3695459c8ec4494eabbe3ea8b7ae3429
Pedro Antonio <pedro.gongora@nextia.mx>
2022-09-08 17:53:13 -0500

    added initial screens¨git push

---
+++ Aliento App/Features/Home/Home.swift
@@ -0,0 +1,24 @@
+//
+//  Home.swift
+//  Aliento App
+//
+//  Created by Pedro Antonio Góngora Sepúlveda on 08/09/22.
+//
+
+import UIKit
+
+class Home: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Home"
+        
+    }
+    
+    @IBAction func goToPredicas(_ sender: Any) {
+        navigationController?.pushViewController(Predicas(), animated: true)
+    }
+}
+        
+
---
+++ Aliento App/Features/Predicas/Predicas.swift
@@ -0,0 +1,13 @@
+//
+//  Predicas.swift
+//  Aliento App
+//
+//  Created by Pedro Antonio Góngora Sepúlveda on 08/09/22.
+//
+
+import UIKit
+
+class Predicas: UIViewController {
+    
+    
+}
