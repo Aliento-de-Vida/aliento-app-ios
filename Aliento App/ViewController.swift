b5fe9135114782255835bd94a36c29635997ff79
Armando Frías <armando_ramirez097@hotmail.com>
2022-09-08 15:43:11 -0500

    created project

---
+++ Aliento App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Aliento App
+//
+//  Created by Juan Armando Frías Ramírez on 08/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
