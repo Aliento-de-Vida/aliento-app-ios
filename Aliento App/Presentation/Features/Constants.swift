78e6355ac5da215ef8c8f2c6c7b9e3a1572c9bbe
Pedro Antonio <pegongorasep@gmail.com>
2022-11-09 20:46:06 -0600

    added sermons collection view

---
+++ Aliento App/Presentation/Features/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Utils.swift
+//  Aliento App
+//
+//  Created by Pedro Antonio on 09/11/22.
+//
+
+import UIKit
+
+public struct Constants {
+    public static let SCREEN_WIDTH = UIScreen.main.bounds.size.width
+}
