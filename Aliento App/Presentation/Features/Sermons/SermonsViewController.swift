1dadab76fc68c91667d00c75c8671e921cb42e94
Pedro Antonio <pegongorasep@gmail.com>
2022-11-10 09:35:13 -0600

    renamed some files

--- Aliento App/Presentation/Features/Sermons/SermonsViewController.swift
+++ Aliento App/Presentation/Features/Sermons/SermonsViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class Predicas: UIViewController {
+class SermonsViewController: UIViewController {
     
     @IBOutlet weak var sermonsCollectionView: SermonsCollectionView!
     
