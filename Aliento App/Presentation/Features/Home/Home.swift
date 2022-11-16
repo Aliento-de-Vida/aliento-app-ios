4d4bff661e555cab89f4569242b856dd16ed900d
Pedro Antonio <pegongorasep@gmail.com>
2022-11-15 20:50:03 -0600

    home ui

--- Aliento App/Presentation/Features/Home/Home.swift
+++ Aliento App/Presentation/Features/Home/Home.swift
@@ -9,20 +9,46 @@ import UIKit
 
 class Home: UIViewController {
     
+    @IBOutlet weak var cardOneImage: UIImageView!
+    @IBOutlet weak var cardTwoImage: UIImageView!
+    @IBOutlet weak var cardThreeImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        HomeRepository.getHome() { (result) in
-            print(result)
-        }
+        HomeRepository.getHome() { (result) in }
         
-        self.title = "Test 4"
+        cardOneImage.load("https://todoserver-peter.herokuapp.com/predicas.jpeg")
         
+        setupNavBar()
+    }
+    
+    func setupNavBar() {
+        setNavBarLogo()
+        setupNavBarLefItem()
+    }
+    
+    func setNavBarLogo() {
+        let image = UIImage(named: "logo_blanco")!.withTintColor(UIColor.black, renderingMode: .alwaysOriginal)
+        let imageView = UIImageView(image: image)
+        imageView.contentMode = UIView.ContentMode.scaleAspectFit
+        
+        self.navigationItem.titleView = imageView
+    }
+    
+    func setupNavBarLefItem() {
+        let image = UIImage(systemName: "gearshape.fill")!.withTintColor(UIColor.black, renderingMode: .alwaysOriginal)
+        
+        let leftBarButton = UIBarButtonItem(image: image, style: .plain, target: self, action: #selector(goToSettings))
+        self.navigationItem.leftBarButtonItem = leftBarButton
     }
     
     @IBAction func goToPredicas(_ sender: Any) {
         navigationController?.pushViewController(SermonsViewController(), animated: true)
     }
-}
+    
+    @objc func goToSettings() {
         
+    }
 
+}
--- Aliento App/Presentation/Features/Sermons/SermonsViewController.swift
+++ Aliento App/Presentation/Features/Sermons/SermonsViewController.swift
@@ -16,6 +16,11 @@ class SermonsViewController: UIViewController {
         Sermon(name: "predica 2", duration: 5000),
         Sermon(name: "predica 3", duration: 8000),
         Sermon(name: "predica 4", duration: 9000),
+        Sermon(name: "predica 5", duration: 9000),
+        Sermon(name: "predica 6", duration: 9000),
+        Sermon(name: "predica 7", duration: 9000),
+        Sermon(name: "predica 8", duration: 9000),
+        Sermon(name: "predica 9", duration: 9000),
     ]
     
     override func viewDidLoad() {
