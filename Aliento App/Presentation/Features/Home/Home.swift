c5506d7617239ce4fd1ba975d4afebcb314d316b
Armando Frías <friasarmando97@outlook.com>
2023-01-10 10:24:46 -0600

    actual

--- Aliento App/Presentation/Features/Home/Home.swift
+++ Aliento App/Presentation/Features/Home/Home.swift
@@ -14,6 +14,8 @@ class Home: UIViewController {
     @IBOutlet weak var cardThreeImage: UIImageView!
     
     @IBOutlet var quickAccessOne: UIImageView!
+    @IBOutlet var quickAccesTwo: UIImageView!
+    @IBOutlet var quickAccesThree: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,13 +23,20 @@ class Home: UIViewController {
         HomeRepository.getHome() { (result) in }
         
         cardOneImage.load(url: "https://concepto.de/wp-content/uploads/2015/03/paisaje-e1549600034372.jpg")
+        cardOneImage.isUserInteractionEnabled = true
+        cardOneImage.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(cardOneClick)))
+        
         cardTwoImage.load(url: "https://img.freepik.com/foto-gratis/silueta-mujer-manos-levantadas_1150-354.jpg?1&w=2000&t=st=1673280205~exp=1673280805~hmac=3d82da85635bea44b6c8f0461f5f4469256cb8c44b57f005a9e5019d0b676784")
         cardThreeImage.load(url: "https://img.freepik.com/foto-gratis/vida-cristiana-crisis-oracion-dios_1150-12938.jpg?w=2000&t=st=1673281128~exp=1673281728~hmac=74e2f4535cdc2418307aa3da83426170ac2d465622d161a977a38bdcdf1e18cf")
         
         
         quickAccessOne.load(url:"https://img.freepik.com/foto-gratis/gente-feliz-apilando-comunidad_1150-1689.jpg?w=2000&t=st=1673290399~exp=1673290999~hmac=ca9a255747e3908c907837f7aa1c13281665b26ccb21a5b5a4606b42874fa47d")
+        quickAccesTwo.load(url: "https://img.freepik.com/foto-gratis/cantante-actuando-escenario-show-vivo-efecto-exposicion-doble-color_53876-104901.jpg?w=2000&t=st=1673365190~exp=1673365790~hmac=41b23af84513549405fa01261aca1b8cdf817cbc8ab98ba8e5346c772a794854")
+        quickAccesThree.load(url: "https://img.freepik.com/foto-gratis/vista-frontal-familia-sonriente-afuera_23-2149661362.jpg?w=1800&t=st=1673365559~exp=1673366159~hmac=2540a65b7dcb151d1ab0e69b99e44089d6cecb4ef183f912bbc3e9c07835da9c")
                 
         setupNavBar()
+        
+
     }
     
     func setupNavBar() {
@@ -57,5 +66,12 @@ class Home: UIViewController {
     @objc func goToSettings() {
         
     }
-
+    
+    @objc func cardOneClick() {
+        print("Se presiono card one")
+        // handling code
+    }
+    
 }
+
+                                      
