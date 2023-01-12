d60386fbab6bee9757bccf43f882df780ea8c8ae
Pedro Antonio <pegongorasep@gmail.com>
2023-01-12 13:22:40 -0600

    updated

---
+++ Aliento App/Presentation/Features/Views/Gradient.swift
@@ -0,0 +1,21 @@
+//
+//  Gradient.swift
+//  Aliento App
+//
+//  Created by Pedro Antonio on 12/01/23.
+//
+
+import UIKit
+
+@IBDesignable class GradientView: UIView {
+    @IBInspectable var topColor: UIColor = UIColor.white
+    @IBInspectable var bottomColor: UIColor = UIColor.black
+
+    override class var layerClass: AnyClass {
+        return CAGradientLayer.self
+    }
+
+    override func layoutSubviews() {
+        (layer as! CAGradientLayer).colors = [topColor.cgColor, bottomColor.cgColor]
+    }
+}
