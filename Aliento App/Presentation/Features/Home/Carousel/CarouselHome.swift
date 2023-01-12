4921ea628ec4b73dbc532ac969fadb427aa64d3d
Armando Frías <armando_ramirez097@hotmail.com>
2023-01-12 12:02:26 -0600

    Updates

---
+++ Aliento App/Presentation/Features/Home/Carousel/CarouselHome.swift
@@ -0,0 +1,14 @@
+//
+//  Carouselitem.swift
+//  Aliento App
+//
+//  Created by Juan Armando Frías Ramírez on 11/01/23.
+//
+
+import Foundation
+
+struct CarouselItem{
+    let imageUrl: String
+    let videoUrl: String?
+    let menuName: String?
+}
---
+++ Aliento App/Presentation/Features/Home/Carousel/CarouselItemCell.swift
@@ -0,0 +1,8 @@
+//
+//  CarouselItemCell.swift
+//  Aliento App
+//
+//  Created by Juan Armando Frías Ramírez on 12/01/23.
+//
+
+import Foundation
