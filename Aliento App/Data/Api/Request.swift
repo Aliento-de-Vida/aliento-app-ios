ad1c0a46b76669232ca4f98b2725ad6161871bbd
Pedro Antonio <pegongorasep@gmail.com>
2023-02-27 10:55:30 -0600

    modified Api class

---
+++ Aliento App/Data/Api/Request.swift
@@ -0,0 +1,29 @@
+//
+//  Request.swift
+//  Aliento App
+//
+//  Created by Pedro Antonio on 24/02/23.
+//
+
+import Foundation
+import Alamofire
+
+public protocol APIConfiguration: URLRequestConvertible {
+    var method: HTTPMethod { get }
+    var path: String { get }
+    var encoding: ParameterEncoding { get }
+    var parameters: Parameters? { get }
+}
+
+struct Request: APIConfiguration {
+    func asURLRequest() throws -> URLRequest {
+        var urlRequest = try URLRequest(url: APIManager.baseUrl + APIVersion.v1.rawValue + path, method: method)
+        urlRequest = try encoding.encode(urlRequest, with: parameters)
+        return urlRequest
+    }
+    
+    var method: HTTPMethod
+    var path: String
+    var encoding: ParameterEncoding = URLEncoding.default
+    var parameters: Parameters? = nil
+}
